d2af778c94768ea85fba2c5ca9ff0222415d7c2b
Sia Hua Jiuh <ahjiuhdotcom@gmail.com>
2017-04-27 23:48:22 +0000

    heroku db debug 3

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,7 @@ var mongoose = require('mongoose');
 // Tell mongoose whenever using promise, use global.Promise
 mongoose.Promise = global.Promise;
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/TodoApp');
-mongoose.connect('mongodb://heroku_mvpfcp81:n3j21f2srv8ssmn1ksflval9p@ds123351.mlab.com:23351/heroku_mvpfcp81' || 'mongodb://localhost/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/TodoApp');
 
 module.exports = {
     mongoose
