3a8905ba98600298fa09b0000f370cac134124e2
Sia Hua Jiuh <ahjiuhdotcom@gmail.com>
2017-05-02 05:50:26 +0000

    Add GET/users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -43,7 +43,7 @@ var UserSchema = new mongoose.Schema({
 // Andrew 2: It's automatically called when we respond to the express request with res.send. 
 // That converts our object to a string by calling JSON.stringify. 
 // JSON.stringify is what calls toJSON.
-// Sia: We are actually overriding .toJSON() method
+// Sia: We are actually overiding .toJSON() method
 UserSchema.methods.toJSON = function() {
     var user = this;
     // 'user.toObject()' taking mongoose variable and convert it to regular object
@@ -67,6 +67,29 @@ UserSchema.methods.generateAuthToken = function() {
   // two 'return'
   // user.save returns a promise. This allows us to attach then/catch calls to do something when the user is save or the save fails. 
   // By returning token, we're enabling whoever calls generateAuthToken to get that value. 
+};
+
+// '.statics' create Model method
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+  
+  try {
+    decoded = jwt.verify(token, 'abc123');    
+  } catch(e) {
+    // return new Promise((resolve, reject) => {
+    //     reject();
+    // });
+    return Promise.reject();
+  }
+  
+  // quote '' is required when we have .xxx for key
+  return User.findOne({
+     '_id': decoded._id,
+     'tokens.token': token,
+     'tokens.access': 'auth'
+  });
+  
 };
 
 var User = mongoose.model('User', UserSchema);
