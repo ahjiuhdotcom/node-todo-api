3ca18d1544f40eef7140ad2096f73bb185db41ae
Sia Hua Jiuh <ahjiuhdotcom@gmail.com>
2017-05-02 07:12:38 +0000

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -3,7 +3,23 @@
 
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123bac';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+      console.log(hash); 
+    });
+});
+
+var hashedPassword = '$2a$10$AFp3ENMm0o4hBBVjvf1RiuIcY3AGw6LmRghw11FGwJ5Elg19vWs6m';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+   console.log(res); 
+});
+
+/*
 var data = {
     id: 10
 };
@@ -14,6 +30,7 @@ console.log(token);
 
 var decoded = jwt.verify(token, '123abc');
 console.log('decoded', decoded);
+*/
 
 /*
 // EXPLANATION OF HASHING AND SALTING
