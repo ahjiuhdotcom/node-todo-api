2c882dab0eeb3fafee8e62362f2c69a8226f395b
Sia Hua Jiuh <ahjiuhdotcom@gmail.com>
2017-05-05 05:11:58 +0000

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -16,9 +16,10 @@ const port = process.env.PORT;
 // get the json string and convert it to js object (in req.boy)
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     var todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user._id
     });
     
     todo.save().then((doc) => {
@@ -28,8 +29,8 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.get('/todos', (req, res) => {
-   Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+   Todo.find({_creator: req.user._id}).then((todos) => {
       // send back the information in object {} form
       // give us more flexibility e.g. allow us to 
       // add other property to it later 
@@ -39,14 +40,17 @@ app.get('/todos', (req, res) => {
    }); 
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
    var id = req.params.id;
 
    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
     
-    Todo.findById(id).then((todo) => {
+    Todo.findOne({
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
@@ -61,14 +65,17 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
 
    if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
     
-    Todo.findByIdAndRemove(id).then((todo) => {
+    Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
@@ -81,7 +88,7 @@ app.delete('/todos/:id', (req, res) => {
 });
 
 // TO UPDATE
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
    var id = req.params.id;
 
    // pick property 'test', 'completed' from req.body
@@ -101,8 +108,8 @@ app.patch('/todos/:id', (req, res) => {
     }
     
     // {new: true} to to set the return object (in return promise, e.g. 'todo' item)
-    // is updated oject or original object. 'new: true' means updated object
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    // is updated object or original object. 'new: true' means updated object
+    Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
         if(!todo) {
             return res.status(404).send();
         }
